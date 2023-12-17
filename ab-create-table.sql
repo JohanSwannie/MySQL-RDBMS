e6367c595d49cb9df5af521e411587e919be10dc
Johan Swan <jswannieman@gmail.com>
2023-12-18 11:36:30 +1300

    Commit ab-create-table SQL Component

---
+++ ab-create-table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS checklists (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+  task_id INT, 
+  title VARCHAR(255) NOT NULL, 
+  is_completed BOOLEAN NOT NULL DEFAULT FALSE, 
+  PRIMARY KEY (id, task_id), 
+  CONSTRAINT task_id_fk
+  FOREIGN KEY (task_id) 
+      REFERENCES tasks (id) 
+      ON UPDATE RESTRICT 
+      ON DELETE CASCADE
+);
