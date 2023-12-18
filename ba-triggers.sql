87e949366aac315ac2a7003b21e9cdee1385b415
Johan Swan <jswannieman@gmail.com>
2023-12-18 22:18:22 +1300

    Commit ba-triggers SQL Component

--- ba-triggers.sql
+++ ba-triggers.sql
@@ -218,4 +218,49 @@ DELETE FROM salaries;
 
 SELECT * FROM SalaryArchives;
 
+-- ********************************
+-- ****      EXAMPLE 5        *****
+-- ****      After Delete    *****
+-- ********************************
+
+DROP TABLE IF EXISTS Salaries;
+
+CREATE TABLE Salaries (
+    employeeNumber INT PRIMARY KEY,
+    salary DECIMAL(10,2) NOT NULL DEFAULT 0
+);
+
+INSERT INTO Salaries(employeeNumber,salary)
+VALUES
+    (1002,5000),
+    (1056,7000),
+    (1076,8000);
+    
+DROP TABLE IF EXISTS SalaryBudgets;
+
+CREATE TABLE SalaryBudgets(
+    total DECIMAL(15,2) NOT NULL
+);
+
+INSERT INTO SalaryBudgets(total)
+SELECT SUM(salary) 
+FROM Salaries;
+
+SELECT * FROM SalaryBudgets;  
+
+CREATE TRIGGER after_salaries_delete
+AFTER DELETE
+ON Salaries FOR EACH ROW
+UPDATE SalaryBudgets 
+SET total = total - old.salary;
+
+DELETE FROM Salaries
+WHERE employeeNumber = 1002;
+
+SELECT * FROM SalaryBudgets;  
+
+DELETE FROM Salaries;
+
+SELECT * FROM SalaryBudgets;    
+
      
