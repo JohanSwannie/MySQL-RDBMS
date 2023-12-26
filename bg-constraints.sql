b50f6b5e00752eb6416ae76fcb944ff824e1698a
Johan Swan <jswannieman@gmail.com>
2023-12-27 12:35:44 +1300

    Update bg-constraints SQL Component

--- bg-constraints.sql
+++ bg-constraints.sql
@@ -104,3 +104,14 @@ CREATE TABLE Persons (
     Age int,
     CHECK (Age>=18)
 );
+
+-- OR
+
+CREATE TABLE Persons (
+    ID int NOT NULL,
+    LastName varchar(255) NOT NULL,
+    FirstName varchar(255),
+    Age int,
+    City varchar(255),
+    CONSTRAINT CHK_Person CHECK (Age>=18 AND City='Sandnes')
+);
