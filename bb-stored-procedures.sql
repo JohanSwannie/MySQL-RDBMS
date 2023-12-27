51f84de757e9e1df5c8387ceaf8e78af4ad93df7
Johan Swan <jswannieman@gmail.com>
2023-12-27 14:04:55 +1300

    Update bb-stored-procedures SQL Component

--- bb-stored-procedures.sql
+++ bb-stored-procedures.sql
@@ -54,4 +54,28 @@ END$$
 
 DELIMITER ;
 
+CALL GetOrderCountByStatus('Shipped',@total);
+SELECT @total;
+
+-- THE FOLLOWING EXAMPLE DEMONSTRATES HOW TO USE 
+-- AN INOUT PARAMETER IN A STORED PROCEDURE:
+
+DELIMITER $$
+
+CREATE PROCEDURE SetCounter(
+	INOUT counter INT,
+    IN inc INT
+)
+BEGIN
+	SET counter = counter + inc;
+END$$
+
+DELIMITER ;
+
+SET @counter = 1;
+CALL SetCounter(@counter,1); -- 2
+CALL SetCounter(@counter,1); -- 3
+CALL SetCounter(@counter,5); -- 8
+SELECT @counter; -- 8
+
 
