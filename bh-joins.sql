a16c71c2712c510223f415e6196166d89788bf52
Johan Swan <jswannieman@gmail.com>
2023-12-28 09:05:35 +1300

    Commit bh-joins SQL Component

--- bh-joins.sql
+++ bh-joins.sql
@@ -43,4 +43,48 @@ FROM
 INNER JOIN committees c USING(name);
 
 
+-- LEFT JOIN
+
+SELECT 
+    m.member_id, 
+    m.name AS member, 
+    c.committee_id, 
+    c.name AS committee
+FROM
+    members m
+LEFT JOIN committees c USING(name);
+
+-- OR
+
+SELECT 
+    m.member_id, 
+    m.name AS member, 
+    c.committee_id, 
+    c.name AS committee
+FROM
+    members m
+LEFT JOIN committees c USING(name)
+WHERE c.committee_id IS NULL;
+
+-- RIGHT JOIN
+
+SELECT 
+    m.member_id, 
+    m.name AS member, 
+    c.committee_id, 
+    c.name AS committee
+FROM
+    members m
+RIGHT JOIN committees c on c.name = m.name;
+
+-- OR
+
+SELECT 
+    m.member_id, 
+    m.name AS member, 
+    c.committee_id, 
+    c.name AS committee
+FROM
+    members m
+RIGHT JOIN committees c USING(name);
 
